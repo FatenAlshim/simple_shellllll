84c6a1629e4ab67b88b45069d3c5cb0286dd0369
purples-dates <purples.dates0s@icloud.com>
2023-10-06 20:29:47 +0000

    add 3rd func

--- simple_ataio.c
+++ simple_ataio.c
@@ -7,10 +7,10 @@
  */
 int is_delim(char c, char *delim)
 {
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
+while (*delim)
+if (*delim++ == c)
+return (1);
+return (0);
 }
 /**
  * interactive - returnss true at shell >> interactive mode
@@ -20,6 +20,33 @@ int is_delim(char c, char *delim)
  */
 int interactive(info_t *info)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
-
+/**
+ * _atoi - converts string to integerr
+ * @s: string to be convertedd
+ * Return: 0 if no no.in string, converted no. otherwise
+ */
+int _atoi(char *s)
+{
+int i, sign = 1, flag = 0, output;
+unsigned int result = 0;
+for (i = 0; s[i] != '\0' && flag != 2; i++)
+{
+if (s[i] == '-')
+sign *= -1;
+if (s[i] >= '0' && s[i] <= '9')
+{
+flag = 1;
+result *= 10;
+result += (s[i] - '0');
+}
+else if (flag == 1)
+flag = 2;
+}
+if (sign == -1)
+output = -result;
+else
+output = result;
+return (output);
+}
