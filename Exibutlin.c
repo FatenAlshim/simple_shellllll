6add75a68282ff79b408beed6b3d917e4aaf8b79
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-06 15:10:15 -0700

    push new c file

--- Exibutlin.c
+++ Exibutlin.c
@@ -6,8 +6,16 @@
  * constant function prototype.
  * Return: Always 0
  */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
 
-
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
+}
 /**
  * _myexit - get out of the shell
  * @info: Struct contain estimated aurgements. Used to maintaing
