8969ca5962bd610d649ea17d0c01f7442396727a
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-06 15:00:58 -0700

    push new c file

---
+++ Exibutlin.c
@@ -0,0 +1,9 @@
+#include "shell.h"
+
+/**
+ * _myexit - get out of the shell
+ * @info: Struct contain estimated aurgements. Used to maintaing
+ * const function datatype.
+ * Return: exits with a given status.
+ * (0) if info.argv[0] != "exit"
+ */
