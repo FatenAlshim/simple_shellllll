009356564feaaee6ab132958b07353cc746df190
purples-dates <purples.dates0s@icloud.com>
2023-10-07 15:28:37 +0000

    addsecfile

--- exibutlin1.c
+++ exibutlin1.c
@@ -8,18 +8,17 @@
  */
 int unset_alias(info_t *info, char *str)
 {
-	char *p, c;
-	int ret;
-
-	p = _strchr(str, '=');
-	if (!p)
-		return (1);
-	c = *p;
-	*p = 0;
-	ret = delete_node_at_index(&(info->alias),
-		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
-	*p = c;
-	return (ret);
+char *p, c;
+int ret;
+p = _strchr(str, '=');
+if (!p)
+return (1);
+c = *p;
+*p = 0;
+ret = delete_node_at_index(&(info->alias),
+get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+*p = c;
+return (ret);
 }
 /**
  * print_alias - printss an alias string
@@ -29,18 +28,48 @@ int unset_alias(info_t *info, char *str)
  */
 int print_alias(list_t *node)
 {
-	char *p = NULL, *a = NULL;
-
-	if (node)
-	{
-		p = _strchr(node->str, '=');
-		for (a = node->str; a <= p; a++)
-		_putchar(*a);
-		_putchar('\'');
-		_puts(p + 1);
-		_puts("'\n");
-		return (0);
-	}
-	return (1);
+char *p = NULL, *a = NULL;
+if (node)
+{
+p = _strchr(node->str, '=');
+for (a = node->str; a <= p; a++)
+_putchar(*a);
+_putchar('\'');
+_puts(p + 1);
+_puts("'\n");
+return (0);
+}
+return (1);
+}
+/**
+ * _myalias - mimicss the alias builtin 
+ * @info: Structure containing potentiall arguments. Used to maintain
+ *          constant function prototypee.
+ *  Return: Always 0
+ */
+int _myalias(info_t *info)
+{
+int i = 0;
+char *p = NULL;
+list_t *node = NULL;
+if (info->argc == 1)
+{
+node = info->alias;
+while (node)
+{
+print_alias(node);
+node = node->next;
+}
+return (0);
+}
+for (i = 1; info->argv[i]; i++)
+{
+p = _strchr(info->argv[i], '=');
+if (p)
+set_alias(info, info->argv[i]);
+else
+print_alias(node_starts_with(info->alias, info->argv[i], '='));
+}
+return (0);
 }
 
