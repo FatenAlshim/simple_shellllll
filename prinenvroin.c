d681789000b2e2efa2bd3096cbb00d667d2cb40c
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-07 06:03:04 -0700

    push new file prinenvroin.c

--- prinenvroin.c
+++ prinenvroin.c
@@ -34,3 +34,14 @@ return (0);
  * constant function prototype.
  *  Return: Always 0
  */
+int _mysetenv(info_t *info)
+{
+if (info->argc != 3)
+{
+_eputs("Incorrect number of arguements\n");
+return (1);
+}
+if (_setenv(info, info->argv[1], info->argv[2]))
+return (0);
+return (1);
+}
