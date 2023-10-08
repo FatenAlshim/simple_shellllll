ad2dcdaf1928cfef52e64ed844cc7091210b0259
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-08 02:49:23 -0700

    push new file c

--- strinpuprin1.c
+++ strinpuprin1.c
@@ -6,3 +6,24 @@
  * Return: 0 if no int in string, changed int otherwise
  * -1 on error
  */
+int _erratoi(char *s)
+{
+int i = 0;
+unsigned long int result = 0;
+
+if (*s == '+')
+s++;  /* TODO: why this make main return 255? */
+for (i = 0;  s[i] != '\0'; i++)
+{
+if (s[i] >= '0' && s[i] <= '9')
+{
+result *= 10;
+result += (s[i] - '0');
+if (result > INT_MAX)
+return (-1);
+}
+else
+return (-1);
+}
+return (result);
+}
