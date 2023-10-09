94d58e976b9cf50e7e8ba84c69ac60b881175d2c
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-09 03:48:43 -0700

    push new c file

--- buferGet.c
+++ buferGet.c
@@ -63,3 +63,14 @@ _putchar(BUF_FLUSH);
  *
  * Return: r
  */
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
+{
+ssize_t r = 0;
+
+if (*i)
+return (0);
+r = read(info->readfd, buf, READ_BUF_SIZE);
+if (r >= 0)
+*i = r;
+return (r);
+}
