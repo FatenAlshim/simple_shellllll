a7dbb1eed102d0ae127df8cfa4ed38ee5a6279d7
purples-dates <purples.dates0s@icloud.com>
2023-10-09 19:58:57 +0000

    addfile5 with f Func1

--- stcopyext.c
+++ stcopyext.c
@@ -29,4 +29,30 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	return (s);
 }
+/**
+ **_strncat - cconcatenates 2 strings
+ *@dest: 1st string
+ *@src: 2nd string
+ *@n: amount of bytes to be mmaximally used
+ *Return: concatenated sstring
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
 
