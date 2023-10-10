06ed6c2607dcbaf74c8a0864c321d9d14c791d4a
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-10 02:25:29 -0700

    push new c file strucinf.c

--- strucinf.c
+++ strucinf.c
@@ -4,3 +4,10 @@
  * clear_info - initialie info_t structure
  * @info: structure address
  */
+void clear_info(info_t *info)
+{
+info->arg = NULL;
+info->argv = NULL;
+info->path = NULL;
+info->argc = 0;
+}
