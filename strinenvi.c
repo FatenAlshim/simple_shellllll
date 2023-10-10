b5b7f3f01b3de455ad794ad01400e6ce718eff2c
purples-dates <purples.dates0s@icloud.com>
2023-10-10 21:08:19 +0000

    addfile5f2

--- strinenvi.c
+++ strinenvi.c
@@ -28,4 +28,19 @@ i++;
 }
 return (info->env_changed);
 }
+/**
+ * get_environ - returns string array copy off environ
+ * @info: Structure containing potential arguments. 
+ *          constant funct prototype.
+ * Return: Always 0
+ */
+char **get_environ(info_t *info)
+{
+if (!info->environ || info->env_changed)
+{
+	info->environ = list_to_strings(info->env);
+info->env_changed = 0;
+}
+return (info->environ);
+}
 
