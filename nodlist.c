a6b8189d9ba7b2e1b313c30c9dc8af71c65c1610
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-11 04:00:11 -0700

    push new cfile nodlist.c

--- nodlist.c
+++ nodlist.c
@@ -8,3 +8,27 @@
  *
  * Return: length of list
  */
+list_t *add_node(list_t **head, const char *str, int num)
+{
+list_t *new_head;
+
+if (!head)
+return (NULL);
+new_head = malloc(sizeof(list_t));
+if (!new_head)
+return (NULL);
+_memset((void *)new_head, 0, sizeof(list_t));
+new_head->num = num;
+if (str)
+{
+new_head->str = _strdup(str);
+if (!new_head->str)
+{
+free(new_head);
+return (NULL);
+}
+}
+new_head->next = *head;
+*head = new_head;
+return (new_head);
+}
