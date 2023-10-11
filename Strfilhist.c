219b081f6711409178ed65bc5d425f31348a1135
purples-dates <purples.dates0s@icloud.com>
2023-10-11 15:18:51 +0000

    addfile6f1

---
+++ Strfilhist.c
@@ -0,0 +1,28 @@
+/**
+ * write_history - createss  file, or appends to existing ffile
+ * @info: pparameter struct
+ *
+ * Return: 1 on success, else -1
+ */
+int write_history(info_t *info)
+{
+	ssize_t fd;
+	char *filename = get_history_file(info);
+	list_t *node = NULL;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	free(filename);
+	if (fd == -1)
+		return (-1);
+	for (node = info->history; node; node = node->next)
+	{
+		_putsfd(node->str, fd);
+		_putfd('\n', fd);
+	}
+	_putfd(BUF_FLUSH, fd);
+	close(fd);
+	return (1);
+
