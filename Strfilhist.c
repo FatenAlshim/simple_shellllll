4e23e991f3de29037265eba69e8025e47338e6de
purples-dates <purples.dates0s@icloud.com>
2023-10-11 15:23:28 +0000

    addfile6f4

--- Strfilhist.c
+++ Strfilhist.c
@@ -65,4 +65,22 @@ int renumber_history(info_t *info)
 	}
 	return (info->histcount = i);
 }
+/**
+ * build_history_list - aadds entry to   history linked list
+ * @info: Structure ccontaining potential arguments. 
+ * @buf: buffer
+ * @linecount:  history linecount, histcount
+ *
+ * Return: Always 0
+ */
+int build_history_list(info_t *info, char *buf, int linecount)
+{
+list_t *node = NULL;
+if (info->history)
+node = info->history;
+add_node_end(&node, buf, linecount);
+if (!info->history)
+info->history = node;
+return (0);
+}
 
