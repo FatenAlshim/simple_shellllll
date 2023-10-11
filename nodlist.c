6a5c0bb416e58285ad84094bfcd96d346f0b4f3a
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-11 03:58:04 -0700

    push new cfile nodlist.c

---
+++ nodlist.c
@@ -0,0 +1,10 @@
+#include "shell.h"
+
+/**
+ * add_node - node added to start of the list
+ * @head: address of pointer to node head
+ * @str: node str field
+ * @num: node index
+ *
+ * Return: length of list
+ */
