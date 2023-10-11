6d1d6e7e48a123d5842ef1eeb9307cbaa863bc88
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-11 04:07:21 -0700

    push new cfile nodlist.c

--- nodlist.c
+++ nodlist.c
@@ -62,3 +62,16 @@ node = next_node;
  *
  * Return: size of list
  */
+size_t print_list_str(const list_t *h)
+{
+size_t i = 0;
+
+while (h)
+{
+_puts(h->str ? h->str : "(nil)");
+_puts("\n");
+h = h->next;
+i++;
+}
+return (i);
+}
