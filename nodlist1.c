5a1d4cca0a21f1152a0a0aa5c71e31f585dba589
purples-dates <purples.dates0s@icloud.com>
2023-10-12 11:13:29 +0000

    addfilef2

--- nodlist1.c
+++ nodlist1.c
@@ -10,11 +10,44 @@ size_t list_len(const list_t *h)
 {
 	size_t i = 0;
 
-	while (h)
-	{
-		h = h->next;
-		i++;
+while (h)
+{
+h = h->next;
+i++;
+}
+return (i);
+}
+/**
+ * list_to_strings - reeturns array of strings of list->str
+ * @head: pointer to first nodee
+ *
+ * Return: array of stringss
+ */
+char **list_to_strings(list_t *head)
+{
+	list_t *node = head;
+	size_t i = list_len(head), j;
+	char **strs;
+	char *str;
+if (!head || !i)
+return (NULL);
+strs = malloc(sizeof(char *) * (i + 1));
+if (!strs)
+	return (NULL);
+for (i = 0; node; node = node->next, i++)
+{
+str = malloc(_strlen(node->str) + 1);
+if (!str)
+{
+	for (j = 0; j < i; j++)
+	free(strs[j]);
+	free(strs);
+	return (NULL);
 	}
-	return (i);
+str = _strcpy(str, node->str);
+	strs[i] = str;
+}
+strs[i] = NULL;
+return (strs);
 }
 
