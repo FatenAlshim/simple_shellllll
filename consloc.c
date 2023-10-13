0c99495e262b5084259c941801f9e62f050ec35a
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-13 12:38:54 -0700

    push new file c consloc.c

--- consloc.c
+++ consloc.c
@@ -7,3 +7,11 @@
  * @n: the amount of bytes to be no space avaliable
  * Return: (s) a pointer to the memory area s
  */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+s[i] = b;
+return (s);
+}
