e6e8058f079ca63bf4c12df46afaf0049592553b
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-13 12:36:54 -0700

    push new file c consloc.c

---
+++ consloc.c
@@ -0,0 +1,9 @@
+#include "shell.h"
+
+/**
+ * _memset - take all spaces from memory with a const byte
+ * @s: the pointer to the memory address
+ * @b: the byte to fill *s 
+ * @n: the amount of bytes to be no space avaliable
+ * Return: (s) a pointer to the memory area s
+ */
