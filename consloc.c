32e0d79a3d2a8104cb7a4ebb8c9b8b00fc83f552
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-13 12:40:52 -0700

    push new file c consloc.c

--- consloc.c
+++ consloc.c
@@ -23,3 +23,24 @@ return (s);
  *
  * Return: pointer to th ol'block nam.
  */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+char *p;
+
+if (!ptr)
+return (malloc(new_size));
+if (!new_size)
+return (free(ptr), NULL);
+if (new_size == old_size)
+return (ptr);
+
+p = malloc(new_size);
+if (!p)
+return (NULL);
+
+old_size = old_size < new_size ? old_size : new_size;
+while (old_size--)
+p[old_size] = ((char *)ptr)[old_size];
+free(ptr);
+return (p);
+}
