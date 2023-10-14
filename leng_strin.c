6ba093674ba3394ebc4a9781dd4feb2b9de3db40
Abdulrahman2o2 <abdulrahman.hussienn@gmail.com>
2023-10-14 03:28:15 -0700

    push new file c leng_strin.c

--- leng_strin.c
+++ leng_strin.c
@@ -24,3 +24,14 @@ return (i);
  *
  * Return: pointer to destination buffer
  */
+char *_strcat(char *dest, char *src)
+{
+char *ret = dest;
+
+while (*dest)
+dest++;
+while (*src)
+*dest++ = *src++;
+*dest = *src;
+return (ret);
+}
